26566f2ccbb120837c0f20a147f355709981a577
daviFNascimento <>
2023-10-24 16:42:15 -0300

    Commit inicial

---
+++ src/main/java/com/api/transferencia/monetariabasica/ApiMonetariaBasicaApplication.java
@@ -0,0 +1,13 @@
+package com.api.transferencia.monetariabasica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonetariaBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonetariaBasicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/transferencia/monetariabasica/ApiMonetariaBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.transferencia.monetariabasica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonetariaBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
