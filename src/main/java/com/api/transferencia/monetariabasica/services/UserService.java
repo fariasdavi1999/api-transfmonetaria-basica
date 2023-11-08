788fc02010d25552d918d51968e329005024f6b2
Davi Farias do Nascimento <>
2023-11-08 15:32:06 -0300

    feature: criando metodos de criação de novo usuario e listagem de todos os usuarios

--- src/main/java/com/api/transferencia/monetariabasica/services/UserService.java
+++ src/main/java/com/api/transferencia/monetariabasica/services/UserService.java
@@ -2,6 +2,7 @@ package com.api.transferencia.monetariabasica.services;
 
 import com.api.transferencia.monetariabasica.commons.exceptions.NotFoundException;
 import com.api.transferencia.monetariabasica.commons.exceptions.ValidateTransactionException;
+import com.api.transferencia.monetariabasica.dtos.UserDTO;
 import com.api.transferencia.monetariabasica.models.user.User;
 import com.api.transferencia.monetariabasica.models.user.UserType;
 import com.api.transferencia.monetariabasica.repositories.UserRepository;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -25,11 +27,6 @@ public class UserService {
         this.logger = logger;
     }
 
-    public void saveUser(User user) {
-        this.userRepository.save(user);
-        logger.info("Usuario salvo");
-    }
-
     public void validatesTransaction(User sender, BigDecimal value) {
         logger.info("Validando transaction");
         if (sender.getUserType() == UserType.TRADER) {
@@ -39,6 +36,23 @@ public class UserService {
         if (sender.getBalance().compareTo(value) < 0) {
             throw new ValidateTransactionException("Usuário não tem saldo");
         }
+    }
+
+    public void saveUser(User user) {
+        logger.info("Salvando user");
+        this.userRepository.save(user);
+    }
+
+
+    public User createUser(UserDTO user) {
+        logger.info("Criando novo usuario");
+        User newUser = new User(user);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 
     public User findUserById(UUID id) {
