f7ab757ec70014591d74ba7c33410c5763f84da0
Davi Farias do Nascimento <>
2023-11-08 15:31:15 -0300

    feature: criando construtor com todos os parâmetros para usar o userdto

--- src/main/java/com/api/transferencia/monetariabasica/models/user/User.java
+++ src/main/java/com/api/transferencia/monetariabasica/models/user/User.java
@@ -1,12 +1,11 @@
 package com.api.transferencia.monetariabasica.models.user;
 
+import com.api.transferencia.monetariabasica.dtos.UserDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -35,11 +34,15 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    @JsonProperty()
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private BigDecimal balance;
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        BeanUtils.copyProperties(data, this);
+    }
 }
