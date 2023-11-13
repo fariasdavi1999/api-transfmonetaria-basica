3f8465878db196ccefed802e5675e36b12ce0cdf
Davi Farias do Nascimento <>
2023-11-13 11:59:02 -0300

    Fix: NotFoundException adicionando metodo

--- src/main/java/com/api/transferencia/monetariabasica/commons/exceptions/NotFoundException.java
+++ src/main/java/com/api/transferencia/monetariabasica/commons/exceptions/NotFoundException.java
@@ -8,4 +8,8 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public NotFoundException(String message, Integer statusCode) {
+        // TODO document why this constructor is empty
+    }
 }
