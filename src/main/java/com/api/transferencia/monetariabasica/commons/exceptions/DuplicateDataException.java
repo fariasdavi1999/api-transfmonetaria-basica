05a88b37507e8b6900c74a706bb0196d587b102d
Davi Farias do Nascimento <>
2023-11-14 11:31:11 -0300

    Removendo arquivos não utilizados

--- src/main/java/com/api/transferencia/monetariabasica/commons/exceptions/DuplicateDataException.java
+++
@@ -1,7 +0,0 @@
-package com.api.transferencia.monetariabasica.commons.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public record DuplicateDataException(String message, Integer statusCode) {
-
-}
--- src/main/java/com/api/transferencia/monetariabasica/commons/exceptions/GeneralException.java
+++
@@ -1,4 +0,0 @@
-package com.api.transferencia.monetariabasica.commons.exceptions;
-
-public record GeneralException(String message, Integer statusCode) {
-}
